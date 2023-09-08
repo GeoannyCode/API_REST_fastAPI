885766ab08e0dbb2bd40b6a611c77f592c6f9f3c
Diego Bracero <diegobracero@hotmail.com>
2023-09-07 22:38:53 -0500

    MongoDB Atlas and Mongo localhost

--- db/client.py
+++ db/client.py
@@ -1,18 +1,23 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
 
 # variables para el nombre de usuario y la contraseña
-username = "tu_usuario"
-password = "tu_contraseña"
+username = os.environ.get("USER")
+password = os.environ.get("PASS")
 
-# url = "mongodb+srv://{username}:{password}@cluster0.lwjhtor.mongodb.net/?retryWrites=true&w=majority"
-# client = MongoClient(url, server_api=ServerApi('1'))
-
-client= MongoClient("mongodb://localhost:27017/")
-db_client = client['users']
+if "USER" and "PASS" in os.environ:
+    url = f"mongodb+srv://{username}:{password}@cluster0.lwjhtor.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(url, server_api=ServerApi('1'))
+else: 
+    client= MongoClient("mongodb://localhost:27017/")
 
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+
+db_client = client['users']
+
